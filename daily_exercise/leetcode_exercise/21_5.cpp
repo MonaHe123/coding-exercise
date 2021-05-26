53fe78799ee08d8c79345cded42de587169fd7bb
unknown <1600012917@pku.edu.cn>
2021-05-26 09:37:49 +0800

    daily exercise

--- daily_exercise/leetcode_exercise/21_5.cpp
+++ daily_exercise/leetcode_exercise/21_5.cpp
@@ -424,3 +424,100 @@ public:
         return cnt;
     }
 };
+
+//566：数组操作的基本练习
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        //首先判断能否转化，不能则直接返回空
+        //如果可以转化，那么遍历第一个矩阵同时对第二个矩阵进行填充
+        int m = mat.size();
+        vector<vector<int>> res;
+        int n;
+        if(m==0)
+        return res;
+        n = mat[0].size();
+        if(m*n!=r*c)
+        return mat;
+        res = vector<vector<int>>(r,vector<int>(c));
+        int k1 = 0;
+        int k2 = 0;
+        for(int i = 0;i<m;++i)
+        for(int j = 0;j<n;++j)
+        {
+            //填充完一行
+            if(k2==c)
+            {
+                k1++;
+                k2 = 0;
+            }
+            res[k1][k2] = mat[i][j];
+            k2++;
+        }
+        return res;
+    }
+};
+
+
+//225：使用队列实现栈的功能
+//思路：使用一个队列作为中间的过度
+class MyStack {
+public:
+queue<int> q1;
+queue<int> q2;
+//用两个队列模拟一个栈：将一个队列作为中间的过度
+//两个队列q1和q2，如果q1中有元素那么先将q1中的全部加入到q2中，然后将加入的元素加入q1中，再将q2中加入q1
+    /** Initialize your data structure here. */
+    MyStack() {
+        //这里是初始化，公共结构的声明要在类的申明处
+        //queue<int> q1;
+        //queue<int> q2;
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        while(!q1.empty())
+        {
+            int tmp = q1.front();
+            q2.push(tmp);
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty())
+        {
+            int tmp = q2.front();
+            q1.push(tmp);
+            q2.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int res = q1.front();
+        q1.pop();
+        return res;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        int res = q1.front();
+        return res;
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        if(q1.empty())
+        return true;
+        else
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
